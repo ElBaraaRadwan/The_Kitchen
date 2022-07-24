2de2f352c9571bb21e1bd24c1cfac6871d403853
El--Baraa Radwan <elbaraa.s.radwan@gmail.com>
2022-07-24 16:46:28 +0200

    working on multer upload funcs & creating the uploads folder structure v2

--- Modules/Tip/Routes/Tip.Routes.js
+++ Modules/Tip/Routes/Tip.Routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { uploadRecipe } = require("../../../middlewares/Storge");
+const { uploadTIP } = require("../../../Middlewares/Storge");
 
 const {
   getAllTips,
@@ -11,8 +11,8 @@ const {
 
 router.get("/getAllTips", getAllTips);
 router.get("/getAllTips/getMyTIPs/:id", getMyTIPs);
-router.post("/createTip", uploadRecipe.array("attachment"), createTip);
-router.patch("/editTip/:id", uploadRecipe.array("attachment"), editTip);
+router.post("/createTip", uploadTIP.array("attachment"), createTip);
+router.patch("/editTip/:id", uploadTIP.array("attachment"), editTip);
 router.delete("/deleteTip/:id", deleteTip);
 
 module.exports = router;
